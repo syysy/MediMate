5cfe9d9493cc3b61e8cd56f9c24d454f1c2d378f
E211439A <L4v3r1T3>
2024-02-15 10:03:59 +0100

    fragment_add_medicament1.xml unit tested

---
+++ app/src/androidTest/java/com/example/mms/ui/add/FragmentAddMedicament1Test.kt
@@ -0,0 +1,44 @@
+package com.example.mms.ui.add
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.example.mms.R
+import org.junit.Test
+
+class FragmentAddMedicament1Test {
+
+    @Test
+    fun testEditionNomDuMedicamentVidePresent() {
+        ActivityScenario.launch(AddMedicamentActivity::class.java)
+        onView(withId(R.id.edit_medicament_nom)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_medicament_nom)).check(matches(withText("")))
+    }
+
+    @Test
+    fun testPresenceListeDeroulanteForme() {
+        ActivityScenario.launch(AddMedicamentActivity::class.java)
+        onView(withId(R.id.spinner_forme)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testPresenceListeDeroulanteDosage(){
+        ActivityScenario.launch(AddMedicamentActivity::class.java)
+        onView(withId(R.id.spinner_dosage)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testPresenceBoutonSuivant(){
+        ActivityScenario.launch(AddMedicamentActivity::class.java)
+        onView(withId(R.id.next_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testPresenceBoutonRetour(){
+        ActivityScenario.launch(AddMedicamentActivity::class.java)
+        onView(withId(R.id.back_button)).check(matches(isDisplayed()))
+    }
+}
