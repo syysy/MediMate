7b77a8b94bb498cbf7f4499279055b7892a55fd4
E211439A <L4v3r1T3>
2024-02-27 11:37:59 +0100

    MedicineStorageDAOTest.kt à tester

---
+++ app/src/androidTest/java/com/example/mms/dao/MedicineStorageDAOTest.kt
@@ -0,0 +1,54 @@
+package com.example.mms.dao
+
+import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
+import androidx.room.Query
+import androidx.test.core.app.ApplicationProvider
+import com.example.mms.database.inApp.AppDatabase
+import com.example.mms.database.inApp.SingletonDatabase
+import com.example.mms.model.MedicineStorage
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class MedicineStorageDAOTest {
+    private lateinit var db: AppDatabase
+    private lateinit var medicineStorageDAO: MedicineStorageDAO
+
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = SingletonDatabase.getDatabase(context)
+        medicineStorageDAO = db.medicineStorageDao()
+    }
+
+    @Test
+    fun testInsert() {
+        setUp()
+        val medicineStorage = MedicineStorage(64793681, 10, 1)
+        medicineStorageDAO.insert(medicineStorage)
+        val medicineStorageFromDB = medicineStorageDAO.getMedicineStorageByMedicineId(64793681)
+        assert(medicineStorageFromDB != null)
+        assert(medicineStorageFromDB!!.medicineId == 64793681L)
+        assert(medicineStorageFromDB.storage == 10)
+        assert(medicineStorageFromDB.alertValue == 1)
+        medicineStorageDAO.delete(64793681)
+    }
+
+    @Test
+    fun testInsertError() {
+        val medicineStorage = MedicineStorage(1, 10, 1)
+        assertThrows(SQLiteConstraintException::class.java) {
+            medicineStorageDAO.insert(medicineStorage)
+        }
+    }
+
+    @Test
+    fun testGetMedicineStorageByMedicineIdFail() {
+        setUp()
+        val medicineStorage = medicineStorageDAO.getMedicineStorageByMedicineId(64793681)
+        assert(medicineStorage == null)
+    }
+
+}
+
+@Query("DELETE FROM MedicineStorage WHERE medicineId = :code_cis")
+private fun MedicineStorageDAO.delete(code_cis: Long){}
